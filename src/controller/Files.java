53fff5bded5ef36415898beac4d24d883d805b59
landryalexandre <landry.alexandre@live.ca>
2015-07-28 11:16:27 -0400

    modif...

--- src/controller/Files.java
+++ src/controller/Files.java
@@ -1,5 +1,6 @@
 package controller;
 
+import model.Attack;
 import model.Dinosaure;
 import model.Log;
 import org.jdom.Document;
@@ -22,6 +23,25 @@ import java.util.Observable;
  * Created by ferhat on 18/07/2015.
  */
 public class Files {
+
+    final String SAVEGAME = "savegame";
+    final String USER_NAME = "username";
+    final String DINOSAURE_LIST = "dinosaures";
+    final String LOG_LIST = "logs";
+    final String SINGLE_DINOSAURE = "dinosaure";
+    final String SINGLE_LOG = "log";
+    final String DINO_NAME = "name";
+    final String DINO_LIFE = "lifepoint";
+    final String DINO_STRENGHT = "strenght";
+    final String DINO_SPEED = "speed";
+    final String DINO_DEFENSE = "defense";
+    final String DINO_XP = "xp";
+    final String DINO_FAMILY = "family";
+    final String ATTACK_LIST = "attacks";
+    final String SINGLE_ATTACK = "attack";
+    final String ATTACK_NAME = "name";
+    final String ATTACK_TYPE = "type";
+    final String ATTACK_VALUE = value;
 
     /**
      * Created by ferhat on 18/07/2015.
@@ -57,27 +77,44 @@ public class Files {
      */
     public void writeFile(String userName, ArrayList<Dinosaure> dinos) throws IOException {
      /* Création de tous les éléments */
-        Element racine = new Element("savegame");
-        Element user = new Element("username");
+        Element racine = new Element(SAVEGAME);
+        Element user = new Element(USER_NAME);
         user.setText(userName);
-        Element Dinos = new Element("dinosaures");
+        Element Dinos = new Element(DINOSAURE_LIST);
         racine.addContent(user);
         racine.addContent(Dinos);
 
-        for (int i = 0; i < dinos.size(); i++) {
-            Element Dino1 = new Element("dinosaure");
-            Element name = new Element("name");
-            name.setText(dinos.get(i).getName());
-            Element life = new Element("lifepoint");
-            life.setText(String.valueOf(dinos.get(i).getLifePoint()));
-            Element strenght = new Element("strenght");
-            strenght.setText(String.valueOf(dinos.get(i).getStrenght()));
-            Element  speed = new Element("speed");
-            speed.setText(String.valueOf(dinos.get(i).getSpeed()));
-            Element  defense = new Element("defense");
-            defense.setText(String.valueOf(dinos.get(i).getDefense()));
-            Element  xp = new Element("xp");
-            xp.setText(String.valueOf(dinos.get(i).getXp()));
+        for (Dinosaure dino : dinos) {
+            Element Dino1 = new Element(SINGLE_DINOSAURE);
+            Element name = new Element(DINO_NAME);
+            name.setText(dino.getName());
+            Element life = new Element(DINO_LIFE);
+            life.setText(String.valueOf(dino.getLifePoint()));
+            Element strenght = new Element(DINO_STRENGHT);
+            strenght.setText(String.valueOf(dino.getStrenght()));
+            Element  speed = new Element(DINO_SPEED);
+            speed.setText(String.valueOf(dino.getSpeed()));
+            Element  defense = new Element(DINO_DEFENSE);
+            defense.setText(String.valueOf(dino.getDefense()));
+            Element  xp = new Element(DINO_XP);
+            xp.setText(String.valueOf(dino.getXp()));
+            Element family = new Element(DINO_FAMILY);
+            family.setText(String.valueOf(dino.getFamily()));
+            Element attacks = new Element(ATTACK_LIST);
+            for(Attack _attack : dino.getAttackList())
+            {
+                Element attack = new Element(SINGLE_ATTACK);
+                Element attackName = new Element(ATTACK_NAME);
+                attackName.setText(_attack.getAttackName());
+                Element attackType = new Element(ATTACK_TYPE);
+                attackType.setText(String.valueOf(_attack.getTypeAttack()));
+                Element value = new Element(ATTACK_VALUE);
+                value.setText(String.valueOf(_attack.getValue()));
+                attacks.addContent(attack);
+                attack.addContent(attackName);
+                attack.addContent(attackType);
+                attack.addContent(value);
+            }
             Dinos.addContent(Dino1);
             Dino1.addContent(name);
             Dino1.addContent(life);
@@ -85,6 +122,8 @@ public class Files {
             Dino1.addContent(speed);
             Dino1.addContent(defense);
             Dino1.addContent(xp);
+            Dino1.addContent(family);
+            Dino1.addContent(attacks);
         }
 
 		/* Enregistrment */
