59b78306d350330a0d1fc6b3d340157110c9b8f0
carolinadave <carole.naka@usherbrooke.ca>
2015-08-07 18:31:20 -0400

    classe Affichage

---
+++ src/view/Affichage.java
@@ -0,0 +1,31 @@
+package view;
+/**
+ * 
+ * @author carole
+ *
+ */
+public class Affichage {
+	
+	public static void welcome(){
+		System.out.println("***  WELCOME TO DINOGAME ! ***");
+	}
+	
+	public static void affichage1(){
+		System.out.println("Enter");
+		System.out.println("1 : to start a new game");
+		System.out.println("2 : to load a game");
+		System.out.println("3 : to quit");
+		System.out.println("Enter a number to choose");
+	}
+	
+	public static void affichage2(){
+		System.out.println("Enter");
+		System.out.println("1 : to create a dinosaur");
+		System.out.println("2 : to see all dinosaurs");
+		System.out.println("3 : to see a dinosaur");
+		System.out.println("4 : to breed dinosaurs");
+		System.out.println("5 : to start a combat");
+		System.out.println("6 : to quit");
+	}
+
+}
